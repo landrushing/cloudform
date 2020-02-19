cf95d3f60fdfbb39028ba6e28fc36d5951a6d534
kylemann16 <kyle@hobu.co>
2020-02-19 16:23:46 -0600

    cloudform-types to @landrush

--- packages/cloudform/index.ts
+++ packages/cloudform/index.ts
@@ -1,5 +1,5 @@
-import { Template } from 'cloudform-types'
-export * from 'cloudform-types'
+import { Template } from '@landrush/cloudform-types'
+export * from '@landrush/cloudform-types'
 
 export default function cloudform(template: Template) {
     return JSON.stringify(template, undefined, 2)
